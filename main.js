12497acf71272986e7453d5cd4625a06062c3414
Michael Ashfield <michael@Eds-iMac.local>
2019-06-11 17:30:51 +0100

    added adding symptoms

--- main.js
+++ main.js
@@ -6,13 +6,32 @@ window.onload = function () {
         data: function () {
             return {
                 symptomList: [],
+                symptomTotals: [{
+                    "stealth": 0,
+                    "resistance": 0,
+                    "speed": 0,
+                    "transmission": 0,
+                }],
                 json: null
+
             }
         },
         methods: {
-            add: function(obj){ 
-                this.symptomList.push(obj.symptom); 
-            }
+            add: function (obj) {
+                if (!this.symptomList.includes(obj)) {
+                    if (this.symptomList.length <= 6) {
+                        this.symptomList.push(obj);
+                    }
+                }
+                let stealthTotal = 0;
+                let resistanceTotal = 0;
+                let speedTotal = 0;
+                let transmissionTotal = 0;
+                for (let symptom in this.symptomList) {
+                    stealthTotal += symptom.stealth;
+                    console.table(symptom.stealth);
+                }
+            },
         }
     });
 
